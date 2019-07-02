d680df416c479f7ea1f6fb184e19d67c7fce91e2
Tom Van Mele <vanmelet@ethz.ch>
2019-07-02 12:55:16 +0800

    pca for beam

--- scripts/extend_aligned.py
+++ scripts/extend_aligned.py
@@ -3,17 +3,23 @@ from __future__ import absolute_import
 from __future__ import division
 
 import os
-import compas
-import compas_fofin
+
 from compas.utilities import pairwise
 from compas.geometry import distance_point_point
 from compas.geometry import add_vectors
 from compas.geometry import intersection_line_plane
 from compas.geometry import Frame
 from compas.geometry import bounding_box
+from compas.geometry import cross_vectors
+from compas.geometry import subtract_vectors
+from compas.geometry import normalize_vector
+from compas.geometry import scale_vector
+from compas.rpc import Proxy
 
 from compas_fofin.datastructures import Shell
 from compas_fofin.rhino import ShellArtist
+
+numerical = Proxy('compas.numerical')
 
 # ==============================================================================
 # Initialise
@@ -26,8 +32,16 @@ FILE_I = os.path.join(DATA, 'data.json')
 shell = Shell.from_json(FILE_I)
 
 keys = [268, 258, 115, 106, 114, 269, 281, 145]
-origin = [0, 2.5, 0]
-normal = [0, 1.0, 0]
+
+p1 = shell.vertex_coordinates(keys[1])
+p0 = shell.vertex_coordinates(keys[0])
+xaxis = subtract_vectors(p1, p0)
+zaxis = [0, 0, 1.0]
+yaxis = cross_vectors(zaxis, xaxis)
+
+offset = 0.5
+normal = normalize_vector(yaxis)
+origin = add_vectors(p0, scale_vector(normal, offset))
 plane = (origin, normal)
 
 points = []
@@ -46,15 +60,16 @@ for key in keys:
         'name'  : "{}.{}.anchor".format(shell.name, key)
     })
 
-box = bounding_box(points)
+result = numerical.pca_numpy(points)
+
+print(result[0])
+print(result[1])
+print(result[2])
 
 lines = []
-for i, j in [(0, 1), (1, 5), (5, 4), (4, 0)]:
-    a = box[i]
-    b = box[j]
-    d = distance_point_point(a, b)
-    if d < 0.01:
-        continue
+a = result[0][0]
+for axis in result[1]:
+    b = add_vectors(a, axis)
     lines.append({
         'start' : a,
         'end'   : b,
